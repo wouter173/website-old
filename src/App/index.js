2d7958c9c4f58f3c22a9313fe1190e3f519dcdb5
wouter <wouter173@outlook.com>
2020-05-28 22:05:49 +0200

    gitignore update

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../Header/'
 import Sidebar from '../SideBar/'
 
+import './main.scss'
+
 export default function Main() {
 	return (
 		<div id="Main">
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import './main.scss'
 
 export default function Header() {
 	return (
     <header id="Header">
-      <h1>test</h1>
+      <h1>test... 123456</h1>
     </header>
   );
 }
