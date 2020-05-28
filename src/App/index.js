3223adfd89272a1e7ed0987142678bbc2dcbddca
wouter <wouter173@outlook.com>
2020-05-28 19:00:08 +0200

    main file setup

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header/'
+import Sidebar from '../SideBar/'
+
+export default function Main() {
+	return (
+		<div id="Main">
+			<Sidebar/>
+			<Header/>
+		</div>
+	)
+}
---
+++ src/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './main.scss'
+
+export default function Header() {
+	return (
+    <header id="Header">
+      <h1>test</h1>
+    </header>
+  );
+}
