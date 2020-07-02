e66ac702b1a798fc34446e36ea273b0703dad34f
wouter <wouter173@outlook.com>
2020-07-02 16:06:07 +0200

    contact page done

--- src/global/Item/index.js
+++ src/global/Item/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+
 import Icon from '../Icon'
+
+import './main.scss'
 
 export default function index(props) {
 	return (
-		<div>
-			
+		<div className="Item">
+			<Icon icon={props.icon} huge dark/>
+			<h3><a href={props.link}>{props.name}</a></h3>
+			<a href={props.link} target="_blank" rel="noopener noreferrer">{props.desc} <Icon icon={faExternalLinkSquareAlt} dark/></a>
 		</div>
 	)
 }
