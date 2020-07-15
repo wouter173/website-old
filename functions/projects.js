e901374534ba489e6c4eb8e390717514103ba6ad
wouter <wouter173@outlook.com>
2020-07-15 20:45:53 +0200

    projects function done

--- functions/projects.js
+++ functions/projects.js
@@ -1,6 +1,20 @@
-exports.handler = async (_event, _context, callback) => {
-	callback(
-		null,
-		{ statusCode: 200, body: 'We are now split testing!' }
-	);
+const Mongo = require('mongodb');
+
+const uri = process.env.DATABASE_URI;
+const options = {
+	useUnifiedTopology: true
+};
+
+exports.handler = (_event, _context, callback) => {
+	Mongo.MongoClient.connect(uri, options, (err, client) => {
+		if (err) return callback(err);
+
+		const db = client.db('Website');
+		const collection = db.collection("Projects");
+
+		collection.find({}).toArray((err, docs) => {
+			if (err) return callback(err);
+			return callback(null, { statusCode: 200, body: JSON.stringify(docs) });
+		});
+	});
 } 
