ec26917d9f3562eeb6aeb3322e6fa51ca0d67295
wouter <wouter173@outlook.com>
2020-07-15 22:42:12 +0200

    cache header added to the projects function

--- functions/projects.js
+++ functions/projects.js
@@ -18,7 +18,13 @@ exports.handler = (_event, _context, callback) => {
 		collection.find({}).toArray((err, docs) => {
 			client.close();
 			if (err) return callback(err);
-			return callback(null, { statusCode: 200, body: JSON.stringify(docs) });
+			return callback(null, {
+				statusCode: 200,
+				headers: {
+					'Cache-Control': 'max-age=86400'
+				},
+				body: JSON.stringify(docs)
+			});
 		});
 	});
 } 
