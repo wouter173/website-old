62e5f3666cdc2a78e79c20e84ee70201b2b754b9
wouter <wouter173@outlook.com>
2020-07-22 22:59:40 +0200

    404 error page

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+import Icon from '../../global/Icon';
+
+import './main.scss'
 
 export default function index() {
 	return (
-		<div>
-			
-		</div>
-	)
+    <div id="Error">
+      <div className="wrapper">
+				<div className="logo"></div>
+				<div className="divider"></div>
+        <h1>404 Page not found</h1>
+        <Link to="/"> <Icon icon={ faArrowLeft } /> Back to home.</Link>
+      </div>
+    </div>
+  );
 }
